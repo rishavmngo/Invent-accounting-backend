9c8f2f2c5883cf3c0e07103f196ecc09c6114a8f
rishavmngo <rishavinmngo@gmail.com>
2025-04-12 20:14:38 +0530

    added party route

---
+++ src/features/party/party.route.ts
@@ -0,0 +1,12 @@
+import { Request, Response, Router } from "express";
+import { partyController } from "./party.controller";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.send("Invent API (secure): Route for adding parties");
+});
+
+router.post("/add", partyController.addNewParty);
+
+export default router;
