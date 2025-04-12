abb1c28f67a6edebd87772c9be465c7002418435
rishavmngo <rishavinmngo@gmail.com>
2025-04-12 09:28:04 +0530

    added party table schema

--- src/data/schema.sql
+++ src/data/schema.sql
@@ -8,4 +8,36 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 
+CREATE TYPE stock_transaction_type AS ENUM ('add', 'reduce', 'sale', 'purchase', 'open','payable','receivable');
+
+CREATE TABLE party(
+id serial PRIMARY KEY,
+name VARCHAR (50) NOT NULL,
+contact_number VARCHAR(15),
+billing_address VARCHAR(100),
+email_address VARCHAR(100),
+state VARCHAR(25),
+gst_type VARCHAR(15),
+gstin VARCHAR(15),
+credit_limit int,
+created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+updated_at TIMESTAMP
+)
+
+
+CREATE TABLE party_opening_balance(
+id serial PRIMARY KEY,
+party_id integer,
+TYPE stock_transaction_type NOT NULL,
+amount INT NOT NULL,
+as_of_date TIMESTAMP NOT NULL DEFAULT NOW(),
+created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+updated_at TIMESTAMP,
+CONSTRAINT fk_party_opening_balance
+	FOREIGN KEY(party_id)
+	REFERENCES party(id)
+	ON DELETE CASCADE 
+)
+
+
 
