86b506653b6200eb8d13ee2fff25df6a06b484d9
rishavmngo <rishavinmngo@gmail.com>
2025-04-12 20:13:40 +0530

    added a helper function to dynamically built raw query with variable number of fields

---
+++ src/shared/helper.ts
@@ -0,0 +1,14 @@
+export function prepareInsertParts<T extends object>(
+  obj: T,
+  exclude: (keyof T)[] = [],
+) {
+  const keys = Object.keys(obj).filter(
+    (key) =>
+      obj[key as keyof T] != undefined && !exclude.includes(key as keyof T),
+  );
+
+  const values = keys.map((key) => obj[key as keyof T]);
+
+  const placeholder = keys.map((_, index) => `$${index + 1}`).join(",");
+  return { keys, values, placeholder };
+}
